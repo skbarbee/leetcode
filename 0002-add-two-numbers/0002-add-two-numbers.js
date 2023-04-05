491c2bd890656351454ded3b7080214752471093
skbarbee <barbee.s.k@gmail.com>
2023-04-05 14:09:15 -0400

    Time: 104 ms (61.78%), Space: 46.8 MB (88.42%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -17,30 +17,27 @@ var addTwoNumbers = function(l1, l2) {
     let carry = 0
     
     while( l1 != null || l2 !==null || sum > 0){
-        if(l1 !== null){
+    
+        if(l1 !==null){
             sum = sum + l1.val
-            l1 = l1.next
+            l1= l1.next
         }
         
-        if (l2 !== null ) {
+        if(l2 !==null){
             sum = sum + l2.val
             l2= l2.next
         }
         
-        if (sum >= 10 ){
+        if(sum >=10){
             carry = 1
             sum = sum - 10
         }
     
-        headOfNewListNode.next = new ListNode(sum);
-        headOfNewListNode = headOfNewListNode.next;
-        
+        headOfNewListNode.next = new ListNode(sum)
+        headOfNewListNode = headOfNewListNode.next
         
         sum = carry
         carry = 0
     }
-    
     return newListNode.next
-    
-   
 };
